d189811606277344a9165d09b34a3399342f01d2
a-kontorius <a-kontorius@yandex.ru>
2017-11-29 18:54:06 +0300

    Добавлено решения для задачи 1. Часть Easy выполнена.

--- Python1/Lesson5/hw05_easy.py
+++ Python1/Lesson5/hw05_easy.py
@@ -2,6 +2,21 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
+current_dir = os.getcwd()
+print(current_dir)
+
+for x in range(1,10):
+    new_catalog_name = 'dir_'+str(x)
+    new_dir_path = os.path.join(current_dir,new_catalog_name)
+    try:
+        os.mkdir(new_dir_path)
+        print("Каталог '{0}' успешно создан".format(new_dir_path))
+    except FileExistsError:
+        print("Не могу создать каталог '{}'. Такой каталог уже существует.".format(new_dir_path))
+    except PermissionError:
+        print("Не могу создать каталог '{0}'. Не хватает прав.".format(new_dir_path))
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
