055c987a91442613533e856c8b0e6cdd11b0414e
a-kontorius <a-kontorius@yandex.ru>
2017-11-30 17:04:45 +0300

    Перевел выполение задачи на функции. Ибо нужно импортировать в normal

--- Python1/Lesson5/hw05_easy.py
+++ Python1/Lesson5/hw05_easy.py
@@ -3,12 +3,13 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 import os
-current_dir = os.getcwd()
-print(current_dir)
+import shutil
 
-for x in range(1,10):
-    new_catalog_name = 'dir_'+str(x)
-    new_dir_path = os.path.join(current_dir,new_catalog_name)
+
+def create_folder(folder):
+    current_directory = os.getcwd()
+    new_dir_path = os.path.join(current_directory, folder)
+
     try:
         os.mkdir(new_dir_path)
         print("Каталог '{0}' успешно создан".format(new_dir_path))
@@ -20,26 +21,34 @@ for x in range(1,10):
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-import os
+def show_current_directory():
+    directories = [f for f in os.listdir(os.getcwd()) if os.path.isdir(f)]
+    [print(dir) for dir in directories] if directories else print("Текущая директория не содержит папок")
 
-current_dir = '.'
-directories = [f for f in os.listdir(current_dir) if os.path.isdir(f)]
-print(directories) if len(directories) else print("Текущая директория не содержит папок")
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-import os
-import shutil
+def create_copy_of_file(src_file):
+    file_name, file_extension = os.path.splitext(src_file)
+    current_direcotry = os.getcwd()
+    new_file = os.path.join(current_direcotry, file_name + '_copy' + file_extension)
 
-current_direcotry = os.getcwd()
-current_file = os.path.join(current_direcotry, os.path.basename(__file__))
+    try:
+        dst = shutil.copyfile(current_file, new_file)
+        print("Копия файла создана:", dst)
+    except Exception as e:
+        print("Не могу создать копию файла. Ошибка:", e.args[1])
 
-file_name, file_extension = os.path.splitext(current_file)
-new_file = os.path.join(current_direcotry, file_name + '_copy' + file_extension)
 
-try:
-    dst = shutil.copyfile(current_file, new_file)
-    print("Копия файла создана:", dst)
-except Exception as e:
-    print("Не могу создать копию файла. Ошибка:", e.args[1])
+if __name__ == "__main__":
+    # Задача-1:
+    for x in range(1, 10):
+        create_folder('dir_' + str(x))
+
+    # Задача-2:
+    show_current_directory()
+
+    # Задача-3:
+    src_file = os.path.join(os.getcwd(), os.path.basename(__file__))
+    create_copy_of_file(src_file)
