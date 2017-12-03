f43015afd07660c24debc868d094c5175ee7045b
a-kontorius <a-kontorius@yandex.ru>
2017-12-04 02:30:00 +0300

    EASY. Выполнил 2-ю задачу. Проверить еще раз перед слиянием

--- Python1/Lesson6/python-1-lesson6-easy.py
+++ Python1/Lesson6/python-1-lesson6-easy.py
@@ -72,7 +72,62 @@ class PoliceCar():
     def turn(self, direction):
         pass
 
+
 # Задача - 2
 # Посмотрите на задачу-1 подумайте как выделить общие признаки классов
-
 # в родительский и остальные просто наследовать от него.
+class Car():
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def turn(self, direction):
+        pass
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(self, speed, color, name, is_police)
+        self.cabin_themperature = 20
+
+    def turn_on_air_conditioner(self):
+        self.cabin_themperature -= 5
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(self, speed, color, name, is_police)
+
+    def accelerate(self):
+        self.speed += 10
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(self, speed, color, name, True)
+        self.cargo = False
+
+    def load_cargo(self):
+        self.cargo = True
+
+    def unload_cargo(self):
+        self.cargo = False
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(self, speed, color, name, True)
+        self.siren = False
+
+    def turn_on_siren(self):
+        self.siren = True
+
+    def turn_off_siren(self):
+        self.siren = False
