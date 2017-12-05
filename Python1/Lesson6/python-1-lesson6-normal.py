2856a555feddd7b77d9eb9110b3fb6c9e7a99682
a-kontorius <a.kontorius@yandex.ru>
2017-12-05 17:50:16 +0300

    Костыльно реализована часть normal

--- Python1/Lesson6/python-1-lesson6-normal.py
+++ Python1/Lesson6/python-1-lesson6-normal.py
@@ -10,3 +10,81 @@
 # Функция подсчета урона должна быть инкапсулирована
 # Вам надо описать игровой цикл так же через класс.
 # Создайте экземпляры классов, проведите бой. Кто будет атаковать первым оставляю на ваше усмотрение.
+
+import random
+
+
+class Person:
+    def __init__(self, name, health, damage, armor):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+
+    # атаковать противника
+    def attack(self, armor):
+        enemy_force = armor.damage
+        print("{} наносит удар.".format(self.name))
+        self._damage(enemy_force)
+
+    # посчитать урон
+    def _damage(self, enemy_force):
+        damage = enemy_force - self.armor
+        print("{} получает {} урона.".format(self.name, damage))
+        self.health -= damage
+
+
+class Player(Person):
+    # увернуться
+    def dodge(self):
+        if random.choice([False, True]):
+            print("'{}' уварачивается от удара.".format(self.name))
+            return True
+        return False
+
+    # посчитать урон
+    def _damage(self, enemy_force):
+        # если увернулся, то не получил урона
+        if self.dodge():
+            damage = 0
+            return
+
+        damage = enemy_force - self.armor
+        print("{} получает {} урона.".format(self.name, damage))
+        self.health -= damage
+
+    def get_health(self):
+        return self.health
+
+class Enemy(Person):
+    pass
+
+
+class Battle():
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
+
+    def play(self):
+        while not self.battle_over():
+            self.player.attack(self.enemy)
+            if not self.battle_over():
+                self.enemy.attack(self.player)
+            else:
+                break
+
+    def battle_over(self):
+        if self.player.get_health() > 0 and self.enemy.get_health() <= 0:
+            print("Битва окончена. победил '{}'".format(self.player.name))
+            return True
+        elif self.enemy.get_health() > 0 and self.player.get_health() <= 0:
+            print("Битва окончена. победил '{}'".format(self.enemy.name))
+            return True
+        return False
+
+
+player = Player("добрый рыцарь", 150, 35, 12)
+armor = Player("злой гоблин", 210, 43, 17)
+
+game = Battle(player, armor)
+game.play()
